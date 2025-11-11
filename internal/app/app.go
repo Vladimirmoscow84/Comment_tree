8f594d521aa064ee646e7096be5797d3550d0a41
Vladimirmoscow84 <ccr1@yandex.ru>
2025-11-11 11:30:04 +0300

    app done

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"comment_tree/internal/handlers"
+	"comment_tree/internal/middleware"
 	"comment_tree/internal/service"
 	"comment_tree/internal/storage"
 	"comment_tree/internal/storage/postgres"
@@ -20,12 +22,16 @@ func Run() {
 
 	databaseURI := cfg.GetString("DATABASE_URI")
 	serverAddr := cfg.GetString("SERVER_ADDRESS")
+	if serverAddr == "" {
+		serverAddr = ":8080" //дефолтный порт на случай непрочтения
+	}
 
 	postgresStore, err := postgres.New(databaseURI)
 	if err != nil {
 		log.Fatalf("[app]failed to connect to PG DB: %v", err)
 	}
 	defer postgresStore.Close()
+	log.Println("[app] Connected to Postgres successfully")
 
 	store, err := storage.New(postgresStore)
 	if err != nil {
@@ -33,8 +39,19 @@ func Run() {
 	}
 	log.Println("[app] Unified storage initialized successfully")
 
-	serviceURL := service.New(store)
+	serviceLayer := service.New(store)
 
 	engine := ginext.New("release")
 
+	engine.Use(middleware.LoggerMiddleware())
+	engine.Use(middleware.CORSMiddleware())
+
+	router := handlers.New(engine, serviceLayer, serviceLayer, serviceLayer)
+	router.Routes()
+
+	log.Printf("[app] starting server on %s", serverAddr)
+	err = engine.Run(serverAddr)
+	if err != nil {
+		log.Fatalf("[app] server failed: %v", err)
+	}
 }
